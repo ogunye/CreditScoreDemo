7ce46dc0a4ec3018d9cbbc3f16d4139b4c11937b
Olufikunmi Ogunye <ogunye_kunmi@yahoo.com>
2023-02-24 11:22:22 +0100

    Project Setup
    Web Api created

---
+++ CreditScore-Frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CreditScore_Frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
